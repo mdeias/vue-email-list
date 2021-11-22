a6e049c82d45f9348eb81e2722829d1235ac248b
mdeias <mdeias@icloud.com>
2021-11-22 17:14:57 +0100

    collegamento axios

--- js/script.js
+++ js/script.js
@@ -3,14 +3,24 @@ const app = new Vue({
     el: '#app',
 
     data:{
-
+        eMails:[]
     },
 
     mounted(){
-
+        this.getMail();
     },
 
     methods:{
+
+        getMail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) =>{
+                this.email = response.data.response;
+
+                console.log(this.email);
+            })
+
+        },
 
     }
 }) 
